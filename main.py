eef6228dccdacfed420b805f631213bd428599d9
danielbeale-ITguy <danielbeale719@gmail.com>
2025-09-21 09:59:21 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import sys
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Shot.containers = (shots,updatable,drawable)
+
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        for astros in asteroids:
+            if astros.collison(player):
+                print('Game over!')
+                sys.exit()
+
+
+        for aster in asteroids:
+            for bull in shots:
+                if aster.collison(bull):
+                    aster.split()
+
+
+
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
